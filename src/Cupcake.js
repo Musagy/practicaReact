c7b61a77ffdcc26a180086f017abd49b859fdfd6
Diego Musagy <diego.1203.dm@gmail.com>
2021-07-13 21:17:44 -0500

    notas de pasar componentes por funcion a clase

--- src/Cupcake.js
+++ src/Cupcake.js
@@ -1,10 +1,23 @@
-const Cupcake = ({sabor,color,url}) => {
-    return (
-        <div className="cupcake">
-            <h3>{`Esto es un cupcake sabor ${sabor} de color ${color}`}</h3>
-            <img src={url} alt={`cupcake ${color}`} />
-        </div>
-    )
+import { Component } from "react"
+
+class Cupcake extends Component{
+    render() {
+        return (
+            <div className="cupcake">
+                <h3>{`Esto es un cupcake sabor ${this.props.sabor} de color ${this.props.color}`}</h3>
+                <img src={this.props.url} alt={`cupcake ${this.props.color}`} />
+            </div>
+        )
+    }
 }
+
+// const Cupcake = ({sabor,color,url}) => {
+//     return (
+//         <div className="cupcake">
+//             <h3>{`Esto es un cupcake sabor ${sabor} de color ${color}`}</h3>
+//             <img src={url} alt={`cupcake ${color}`} />
+//         </div>
+//     )
+// }
 
 export default Cupcake
